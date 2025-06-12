79004dc66fcccbcdd48b25e4a42e2c1128a7c3f5
VitaliyBeletskiy <vitaliy.beletskiy@gmail.com>
2025-06-12 13:13:27 +0200

    feat: add Reversi game basic UI

--- feature-reversi-ui/src/main/java/com/beletskiy/reversi/ui/screens/GameScreen.kt
+++ feature-reversi-ui/src/main/java/com/beletskiy/reversi/ui/screens/GameScreen.kt
@@ -1,42 +1,112 @@
 package com.beletskiy.reversi.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.beletskiy.reversi.data.FakeReversiGame
+import com.beletskiy.reversi.ui.components.ReversiAppBar
 import com.beletskiy.shared.theme.GamesTheme
+import com.beletskiy.reversi.ui.R
+import com.beletskiy.reversi.ui.components.BoardView
+import com.beletskiy.reversi.ui.components.ScoreBoardView
+import com.beletskiy.shared.components.TwoButtonsDialog
 
 @Composable
 fun GameScreen(
     viewModel: GameViewModel,
     onMenuClick: () -> Unit = {},
 ) {
-    Scaffold() {
-        Column(
+    val uiState by viewModel.gameUiState.collectAsStateWithLifecycle()
+    var showRestartGameDialog by remember { mutableStateOf(false) }
+
+    if (showRestartGameDialog) {
+        TwoButtonsDialog(
+            title = stringResource(R.string.restart_game),
+            message = stringResource(R.string.are_you_sure_you_want_to_restart_the_game),
+            confirmText = stringResource(R.string.confirm),
+            dismissText = stringResource(R.string.dismiss),
+            onDismissRequest = { showRestartGameDialog = false },
+            onConfirmation = {
+                showRestartGameDialog = false
+                viewModel.newGame()
+            },
+        )
+    }
+
+    var isTheFirstLaunch by rememberSaveable { mutableStateOf(true) }
+    if (isTheFirstLaunch) {
+        isTheFirstLaunch = false
+        LaunchedEffect(Unit) {
+            viewModel.newGame()
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            ReversiAppBar(
+                title = stringResource(R.string.reversi_game_title),
+                onMenu = onMenuClick,
+                onRestartGame = {
+                    showRestartGameDialog = true
+                },
+            )
+        }
+    ) {
+        Box(
             modifier = Modifier
                 .padding(it)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top,
+                .fillMaxSize()
         ) {
-            Text(text = "Reversi Placeholder")
+            key(uiState.gameSessionId) {
+                BoardView(
+                    board = uiState.board,
+                    isGameOver = uiState.isGameOver,
+                    currentDisc = uiState.currentPlayerDisc,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.Center),
+                ) { row, column ->
+                    viewModel.takeTurn(row, column)
+                }
+            }
+
+            ScoreBoardView(
+                blackScore = uiState.blackScore,
+                whiteScore = uiState.whiteScore,
+                currentDisc = uiState.currentPlayerDisc,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.TopCenter)
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+            )
         }
     }
 }
 
 @SuppressLint("ViewModelConstructorInComposable")
-@Preview
+@Preview(showSystemUi = true, showBackground = true, widthDp = 360, heightDp = 722)
 @Composable
 private fun PreviewGameScreen() {
     GamesTheme {
-        val previewViewModel = GameViewModel()
+        val fakeGame = FakeReversiGame()
+        val previewViewModel = GameViewModel(fakeGame)
         GameScreen(viewModel = previewViewModel)
     }
 }
--- feature-reversi-ui/src/main/java/com/beletskiy/reversi/ui/screens/GameViewModel.kt
+++ feature-reversi-ui/src/main/java/com/beletskiy/reversi/ui/screens/GameViewModel.kt
@@ -1,10 +1,54 @@
 package com.beletskiy.reversi.ui.screens
 
 import androidx.lifecycle.ViewModel
+import com.beletskiy.reversi.data.Cell
+import com.beletskiy.reversi.data.IReversiGame
+import com.beletskiy.reversi.data.PlayerDisc
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
-@HiltViewModel
-class GameViewModel @Inject constructor() : ViewModel() {
+data class GameUiState(
+    val gameSessionId: Int = 0,
+    val board: List<List<Cell>> = List(IReversiGame.BOARD_SIZE) { List(IReversiGame.BOARD_SIZE) { Cell() } },
+    val currentPlayerDisc: PlayerDisc = PlayerDisc.BLACK,
+    val blackScore: Int = 0,
+    val whiteScore: Int = 0,
+    val isGameOver: Boolean = false,
+)
 
+@HiltViewModel
+class GameViewModel @Inject constructor(private val reversiGame: IReversiGame) : ViewModel() {
+
+    private val _gameUiState = MutableStateFlow(GameUiState())
+    val gameUiState: StateFlow<GameUiState> = _gameUiState
+
+    fun newGame() {
+        val gameState = reversiGame.newGame()
+        _gameUiState.update {
+            it.copy(
+                gameSessionId = it.gameSessionId + 1,
+                board = gameState.board,
+                currentPlayerDisc = gameState.currentPlayer,
+                blackScore = gameState.blackScore,
+                whiteScore = gameState.whiteScore,
+                isGameOver = gameState.isGameOver,
+            )
+        }
+    }
+
+    fun takeTurn(row: Int, col: Int) {
+        val gameState = reversiGame.makeMove(row, col)
+        _gameUiState.update {
+            it.copy(
+                board = gameState.board,
+                currentPlayerDisc = gameState.currentPlayer,
+                blackScore = gameState.blackScore,
+                whiteScore = gameState.whiteScore,
+                isGameOver = gameState.isGameOver,
+            )
+        }
+    }
 }
